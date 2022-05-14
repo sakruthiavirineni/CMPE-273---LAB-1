75cee9641c7df5c3b7f61f0b570da04f3bcb2f45
sakruthiavirineni <86926099+sakruthiavirineni@users.noreply.github.com>
2022-05-14 08:41:48 -0700

    Updated Favorites

--- backend/models/Favourites.js
+++ backend/models/Favourites.js
@@ -24,3 +24,21 @@ module.exports = (sequelize, DataTypes) => {
   });
   return Favourites;
 };
+const mongoose = require("mongoose");
+const Userdb = require("./model");
+const Items = require("./items");
+
+var schema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: Userdb,
+  },
+  itemId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: Items,
+  },
+});
+
+const Favourites = mongoose.model("Favourites", schema);
+
+module.exports = Favourites;
