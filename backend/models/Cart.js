477ce2188d1d91b14082f2983dc993e58a23fc10
sakruthiavirineni <86926099+sakruthiavirineni@users.noreply.github.com>
2022-05-14 08:41:02 -0700

    Updated Cart.js

--- backend/models/Cart.js
+++ backend/models/Cart.js
@@ -30,3 +30,25 @@ module.exports = (sequelize, DataTypes) => {
   });
   return Cart;
 };
+const mongoose = require("mongoose");
+const Userdb = require("./model");
+const Items = require("./items");
+
+var schema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: Userdb,
+  },
+  itemId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: Items,
+  },
+  qty: {
+    type: Number,
+    default: null,
+  },
+});
+
+const Cart = mongoose.model("Cart", schema);
+
+module.exports = Cart;
